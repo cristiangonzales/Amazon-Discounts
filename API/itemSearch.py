2d3a13213a3fb6db37ea294a2cfdfbb153d706d8
khaihua <33015603+khaihua@users.noreply.github.com>
2017-10-31 22:08:34 -0700

    Add files via upload

---
+++ API/itemSearch.py
@@ -0,0 +1,23 @@
+import amazonproduct
+from amazonproduct import API
+import lxml
+config = {
+'access_key': 'AKIAIKLBJZJSC65EHZAA',
+'secret_key': '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',
+'associate_tag': 'kdhua-20',
+'locale': 'us'
+}
+api = amazonproduct.API(cfg=config)
+
+result = api.item_search('All',Keywords='chair', ResponseGroup='Large')
+
+    # extract paging information
+total_results = result.results
+total_pages = len(result)  # or result.pages
+
+for chairs in result:
+
+    print 'page %d of %d' % (result.current, total_pages)
+    print chairs.ASIN + '\n'
+
+   
