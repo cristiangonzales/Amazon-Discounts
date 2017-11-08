bc61c0241394cd993c057747b817934a4df1dd18
khaihua <33015603+khaihua@users.noreply.github.com>
2017-11-08 14:52:13 -0800

    Update itemLookup.py

--- API/itemLookup.py
+++ API/itemLookup.py
@@ -1,14 +1,8 @@
 import amazonproduct
 from amazonproduct import API
 import lxml
-config = {
-'access_key': 'AKIAIKLBJZJSC65EHZAA',
-'secret_key': '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',
-'associate_tag': 'kdhua-20',
-'locale': 'us'
-}
 def itemLookup(ASIN):
-  api = amazonproduct.API(cfg=config)
+  api = amazonproduct.API(cfg=~/.credential)
   result = api.item_lookup(ASIN)
   for item in result.Items.items:
     print '%s (%s)' % (item.ItemAttributes.Title, item.ItemAttributes.Price)
