4e1cd1db4da88a60e5465565f229f25a1df93642
khaihua <33015603+khaihua@users.noreply.github.com>
2017-11-08 19:59:31 -0800

    Update itemSearch.py

--- API/itemSearch.py
+++ API/itemSearch.py
@@ -1,14 +1,8 @@
 import amazonproduct
 from amazonproduct import API
 import lxml
-config = {
-'access_key': 'AKIAIKLBJZJSC65EHZAA',
-'secret_key': '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',
-'associate_tag': 'kdhua-20',
-'locale': 'us'
-}
 def itemSearch(keywords, pctoff, searchIndex = 'All'):
-    api = amazonproduct.API(cfg=config)
+    api = amazonproduct.API(cfg=~/.credential)
     result = api.item_search(searchIndex,Keywords=keywords, ResponseGroup='Large', MinPercentageOff=pctoff)
     asin_list = []
     for item in result:
