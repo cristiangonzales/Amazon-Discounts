97cfd7c0d8289407f228c74ddc43e75e1f66ac63
nleeds <nleeds@ucsc.edu>
2017-11-08 16:09:21 -0800

    prints URLs and returns a list of ASINs
    itemSearch prints URLs and returns a list of ASINs

--- API/itemSearch.py
+++ API/itemSearch.py
@@ -1,12 +1,26 @@
 import amazonproduct
 from amazonproduct import API
 import lxml
-def item_search(keywords, pctoff, searchIndex = 'All'):
-  api = amazonproduct.API(cfg=~/.credential)
-  result = api.item_search(searchIndex,Keywords=keywords, ResponseGroup='Large', MinPercentageOff=pctoff)
-  total_results = result.results
-  total_pages = len(result)  # or result.pages
-  for items in result:
-    print 'page %d of %d' % (result.current, total_pages)
-    print items.ASIN + '\n'
-    return items.ASIN
+config = {
+'access_key': 'AKIAIKLBJZJSC65EHZAA',
+'secret_key': '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',
+'associate_tag': 'kdhua-20',
+'locale': 'us'
+}
+def itemSearch(keywords, pctoff, searchIndex = 'All'):
+    api = amazonproduct.API(cfg=config)
+    result = api.item_search(searchIndex,Keywords=keywords, ResponseGroup='Large', MinPercentageOff=pctoff)
+    asin_list = []
+    for item in result:
+      asin_list.append(item.ASIN)
+      print (item.DetailPageURL)
+    return asin_list
+
+def main():
+    asin_results = itemSearch('chair', 50)
+    for item in asin_results:
+      print item
+
+if __name__ == '__main__':
+  main()
+  
