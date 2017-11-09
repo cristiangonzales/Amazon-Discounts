f7dceaefa6ef49ac5091071f82b41090b6584578
nleeds <nleeds@ucsc.edu>
2017-11-08 16:07:02 -0800

    Added Title, Price, and URL
    itemLookup takes an ASIN and prints Title, Price, and URL.

--- API/itemLookup.py
+++ API/itemLookup.py
@@ -1,9 +1,26 @@
 import amazonproduct
 from amazonproduct import API
 import lxml
-def itemLookup(ASIN):
-  api = amazonproduct.API(cfg=~/.credential)
-  result = api.item_lookup(ASIN)
-  for item in result.Items.items:
-    print '%s (%s)' % (item.ItemAttributes.Title, item.ItemAttributes.Price)
-    return item.ASIN
+config = {
+'access_key': 'AKIAIKLBJZJSC65EHZAA',
+'secret_key': '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',
+'associate_tag': 'kdhua-20',
+'locale': 'us'
+}
+
+def itemLookup(asin):
+  api = amazonproduct.API(cfg=config)
+  result = api.item_lookup(asin, ResponseGroup= 'Large')
+
+  for item in result.Items.Item:
+    print (item.ItemAttributes.Title)
+    print (item.OfferSummary.LowestNewPrice.FormattedPrice)
+    print (item.DetailPageURL)
+    return (item.ASIN)
+
+  
+def main():
+  itemLookup('B00V2L5JRA')
+
+if __name__ == '__main__':
+  main()
