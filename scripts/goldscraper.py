cc2597c0055fa1f9372bb18eb821532fe3168699
ctfarran <33010129+ctfarran@users.noreply.github.com>
2017-11-29 16:37:28 -0800

    Goldscraper working, not clean
    clean this and test more

--- scripts/goldscraper.py
+++ scripts/goldscraper.py
@@ -1,38 +1,42 @@
-import requests
-from bs4 import BeautifulSoup
 import re, os
+import time
+import random
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 
-def scrape_goldbox(discount):
-	#loop through each page:
-	asins = scrape_page('https://www.amazon.com/gp/goldbox')
-	#hits = {}
-	#for item in asin:
-	#	get_camel_price(item)
-	#	get_amazon_price(item)
-	#	calculate discount
-	#	if itemDiscount >= discount:
-	#		hits.append(item)
-	#return hits
-	print asins
-	return asins
+def unicode_to_ascii(listOfStr):
+	for i in range(len(listOfStr)):
+		asciiStr = listOfStr[i].encode('ascii', errors='backslashreplace')
+		listOfStr[i] = asciiStr
+	return listOfStr
+def scrape_goldbox(numOfPages):
+	driver = webdriver.Chrome()
+	driver.get("https://www.amazon.com/gp/goldbox")
+	asins = []
+	j = 0
+	while(j<numOfPages):
+		strOfContent = driver.page_source
+		#try:
 
-def scrape_page(url):
-	try:
-		response = requests.get(url)
-		html = response.content
-
-		soup = BeautifulSoup(html, "html.parser")
-		strOfContent = str(soup)
-		#pattern = "\"reviewAsin.*\","
+			# for tag in soup.findAll("a", href=True):
+			# 	print tag["href"]
+			# strOfContent = str(soup)
+			#pattern = "\"reviewAsin.*\","
 		pattern = "/dp/[0-9|A-Z]{9,11}"
 		result = re.findall(pattern,strOfContent)
 		for i in range(len(result)):
 			item = result[i].replace("/dp/","")
 			result[i] = item
 		result = list(set(result))
-		#print result
-		return result
-	except Exception as e:
-		print "Error: unable to connect to " + url + "for scraping. Error "
-
-scrape_goldbox(10)
+		asins = asins + result
+		# except Exception as e:
+		# 	print "Error: unable to connect to " + url + "for scraping. Error "
+		next_button = driver.find_elements_by_partial_link_text('Next')[0]
+		driver.execute_script("window.scrollTo(0,4687)")
+		next_button.click()
+		j += 1
+		time.sleep(random.random()*10)
+	print unicode_to_ascii(asins)
+	return unicode_to_ascii(asins)
+#scrape_goldbox(5)
