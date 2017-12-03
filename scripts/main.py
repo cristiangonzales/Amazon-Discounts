9bce294f1c11ca149696c607240467f0a0cb31e3
root <ctfarran@ucsc.edu>
2017-12-02 16:19:37 -0800

    main updated

--- scripts/main.py
+++ scripts/main.py
@@ -19,24 +19,18 @@ def main():
         discount = sys.argv[1]
         outFile = open(sys.argv[2])
         camelObj = AMZNCamelScraper.AMZNCamelScraper()
+        asinList = []
         if isinstance(argv[-1],int):
-            asinList = goldscraper.scrape_goldbox(sys.argv[-1])
-            for asin in asinList:
-                titlePriceURLList = itemLookup.item_lookup(asin)
-                if check_discount(discount,asin,camelObj,titlePriceURLList[1]):
-                    outFile.write("Name: " + titlePriceURLList[0] + 
-                        " Amazon price: " + titlePriceURLList[1] + 
-                        " Camel price " + camelPrice + " discount: " + "URL: " 
-                        + titlePriceURLList[2])
+            asinList = goldscraper.scrape_goldbox(sys.argv[-1]) 
         else:
-            asinResults = itemSearch.item_search(sys.argv[-1], discount)
-            for item in asinResults:
-                titlePriceURLList = itemLookup.item_lookup(asin)
-                if check_discount(discount,asin,camelObj,titlePriceURLList[1]):
-                    outFile.write("Name: " + titlePriceURLList[0] + 
-                        " Amazon price: " + titlePriceURLList[1] + 
-                        " Camel price " + camelPrice + " discount: " + "URL: " 
-                        + titlePriceURLList[2])
+            asinList = itemSearch.item_search(sys.argv[-1], discount)
+        for asin in asinList:
+            titlePriceURLList = itemLookup.item_lookup(asin)
+            if check_discount(discount,asin,camelObj,titlePriceURLList[1]):
+                outFile.write("Name: " + titlePriceURLList[0] + 
+                    " Amazon price: " + titlePriceURLList[1] + 
+                    " Camel price " + camelPrice + " discount: " + "URL: " 
+                    + titlePriceURLList[2])
         outFile.close()
     except:
         print usage
